b8169306237b7a720054d13c0bbfcc7a1c073058
vishSP <hihi64741@gmail.com>
2023-04-04 16:56:40 +0300

    написал 1 домашку

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employees_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date varchar(100) NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(50) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employees_id),
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,43 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+customers_row = []
+employees_row = []
+orders_row = []
+
+with open('north_data/customers_data.csv') as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader)
+    for row in csv_reader:
+        customer_id, company_name, contact_name = row
+        customers_row.append(row)
+
+with open('north_data/employees_data.csv') as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader)
+    for row in csv_reader:
+        first_name, last_name, title, birth_date, notes = row
+        employees_row.append(row)
+
+with open('north_data/orders_data.csv') as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader)
+    for row in csv_reader:
+        order_id, customer_id, employee_id, order_date, ship_city = row
+        orders_row.append(row)
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='1620') as conn:
+    with conn.cursor() as cur:
+        for employee in employees_row:
+            cur.execute('INSERT INTO employees (last_name, first_name, title , birth_date ,notes)'
+                        'VALUES (%s, %s, %s, %s, %s)', employee)
+            conn.commit()
+        for customer in customers_row:
+            cur.execute("INSERT INTO customers VALUES (%s,%s,%s)", customer)
+            conn.commit()
+
+        for order in orders_row:
+            cur.execute("INSERT INTO orders VALUES (%s,%s,%s,%s,%s)", order)
+
+conn.close()
